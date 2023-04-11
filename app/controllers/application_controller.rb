209f19f5683823d46af0005fdc8d48f0a7db7602
Ananth Prayaga <ananthp@uchicago.edu>
2023-04-11 03:42:17 +0000

    Final Push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,42 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render(:template => "game_templates/rules.html.erb")
+  end
+
   def play_rock
     # Write ton of code
     render(:template => "game_templates/user_rock.html.erb")
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+    render(:template => "game_templates/user_paper.html.erb")
+  end
+
+  def play_scissors
+    # Write ton of code
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render(:template => "game_templates/user_scissors.html.erb")
   end
 end
